21027e04f2e227f67f9287f1623d894379c95863
Assam Toumi <assamtomi@outlook.com>
2020-07-03 20:56:48 +0100

    done!

---
+++ DragDropDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace DragDropDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            customerViewSource.Source = DataLayer.GetCustomers();
+            System.Windows.Data.CollectionViewSource statusViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("statusViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            statusViewSource.Source = DataLayer.GetStatuses();
+        }
+    }
+}
---
+++ DragDropDataBinding/MainWindowMVVM.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace DragDropDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowMVVM : Window
+    {
+        public MainWindowMVVM()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // customerViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource statusViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("statusViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // statusViewSource.Source = [generic data source]
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource statusViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("statusViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // statusViewSource.Source = [generic data source]
+        }
+    }
+}
